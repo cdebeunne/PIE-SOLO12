497cee75008944e7f188b67763b81d97bbfa7343
Damien DESCLAUX <Damien.DESCLAUX@student.isae-supaero.fr>
2020-11-02 17:20:28 +0100

    Correction of SOLO12 joint indexes.

--- solo_pybullet/initialization_simulation.py
+++ solo_pybullet/initialization_simulation.py
@@ -41,7 +41,7 @@ def configure_simulation(dt, enableGUI):
 
     # Disable default motor control for revolute joints
     #revoluteJointIndices = [0, 1, 3, 4, 6, 7, 9, 10, 12, 13]
-    revoluteJointIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    revoluteJointIndices = [0,1,2, 4,5,6, 8,9,10, 12,13,14]
     p.setJointMotorControlArray(robotId,
                                 jointIndices=revoluteJointIndices,
                                 controlMode=p.VELOCITY_CONTROL,
