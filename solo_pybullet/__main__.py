7e54393ca978d6cb0b189fa447b945323b234663
Damien DESCLAUX <Damien.DESCLAUX@student.isae-supaero.fr>
2020-11-02 16:35:07 +0100

    Implemented first version of controler.

--- solo_pybullet/__main__.py
+++ solo_pybullet/__main__.py
@@ -16,38 +16,46 @@ from .initialization_simulation import configure_simulation, getPosVelJoints
 #  INITIALIZATION ##
 ####################
 
-dt = 0.0001  # time step of the simulation
+sim_dt = 0.0001  # time step of the simulation
+sim_tfinal = 10 # end time of the simulation
 # If True then we will sleep in the main loop to have a 1:1 ratio of (elapsed real time / elapsed time in the
 # simulation)
-realTimeSimulation = True
+sim_realTime_enable = True
+sim_slowMotion_enable = False
+sim_slowMotion_ratio = 10
 enableGUI = True  # enable PyBullet GUI or not
-robotId, solo, revoluteJointIndices = configure_simulation(dt, enableGUI)
+robotId, solo, revoluteJointIndices = configure_simulation(sim_dt, enableGUI)
 
 ###############
 #  MAIN LOOP ##
 ###############
 
-for i in range(10000):  # run the simulation during dt * i_max seconds (simulation time)
+for i in range(int(sim_tfinal/sim_dt)):  # run the simulation during dt * i_max seconds (simulation time)
 
     # Time at the start of the loop
-    if realTimeSimulation:
+    if sim_realTime_enable or sim_slowMotion_enable:
         t0 = time.clock()
 
     # Get position and velocity of all joints in PyBullet (free flying base + motors)
     q, qdot = getPosVelJoints(robotId, revoluteJointIndices)
 
     # Call controller to get torques for all joints
-    jointTorques = fall(q, solo)
+    jointTorques = jump(q, qdot, solo, sim_dt)
 
     # Set control torques for all joints in PyBullet
     p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
 
     # Compute one step of simulation
     p.stepSimulation()
+    solo.display(q)
 
     # Sleep to get a real time simulation
-    if realTimeSimulation:
-        t_sleep = dt - (time.clock() - t0)
+    if sim_realTime_enable:
+        t_sleep = sim_dt - (time.clock() - t0)
+        if t_sleep > 0:
+            time.sleep(t_sleep)
+    elif sim_slowMotion_enable:
+        t_sleep = sim_slowMotion_ratio*sim_dt - (time.clock() - t0)
         if t_sleep > 0:
             time.sleep(t_sleep)
 
