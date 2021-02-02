7bf74b48ff9bad639ae2b6a014e2f7739b9137e4
Debeunne Cesar <cesar.debeunne@student.isae-supaero.fr>
2021-02-02 10:56:57 +0100

    contact force implemented

--- test_TSID.py
+++ test_TSID.py
@@ -14,12 +14,18 @@ from example_robot_data import loadSolo  # Functions to load the SOLO quadruped
 
 # Load the robot for Pinocchio
 solo12 = loadSolo(False)
-solo12.initDisplay(loadModel=True)
+solo12.initViewer(loadModel=True)
 path = "/opt/openrobots/share/example-robot-data/robots/solo_description/robots"
 urdf = path + "/solo12.urdf"
 vec = pin.StdVec_StdString()
 vec.extend(item for item in path)
 solo12_wrapper = tsid.RobotWrapper(urdf, vec, pin.JointModelFreeFlyer(), False)
+
+# get frame ID
+ID_FL = solo12.model.getFrameId("FL_FOOT")
+ID_FR = solo12.model.getFrameId("FR_FOOT")
+ID_HL = solo12.model.getFrameId("HL_FOOT")
+ID_HR = solo12.model.getFrameId("HR_FOOT")
 
 # formulate the problem
 
@@ -47,7 +53,7 @@ q      = np.full((solo12_wrapper.nq, N_simu + 1), np.nan)
 v      = np.full((solo12_wrapper.nv, N_simu + 1), np.nan)
 dv     = np.full((solo12_wrapper.nv, N_simu + 1), np.nan)
 com_start = np.zeros(3).T
-com_end = np.array([0,0,-0.5]).T
+com_end = np.array([0,0,-1]).T
 com_traj  = np.full((3, N_simu), np.nan)
 comv_traj  = np.full((3, N_simu), np.nan)
 comdv_traj = np.full((3, N_simu), np.nan)
@@ -62,6 +68,41 @@ sampleCom = trajCom.computeNext()
 
 solver = tsid.SolverHQuadProgFast("qp solver")
 solver.resize(formulation.nVar, formulation.nEq, formulation.nIn)
+
+# create contact force
+contactNormal = np.array([0,0,1])
+kp_contact = 30
+mu = 1
+
+contactFL = tsid.ContactPoint("contactFL", solo12_wrapper, "FL_FOOT", contactNormal, mu, 1, 1000)
+contactFL.setKp(kp_contact * np.ones(6).T)
+contactFL.setKd(2*np.sqrt(kp_contact) * np.ones(6).T)
+fl_ref = solo12_wrapper.position(data, ID_FL)
+contactFL.setReference(fl_ref)
+formulation.addRigidContact(contactFL, 10)
+
+contactFR = tsid.ContactPoint("contactFR", solo12_wrapper, "FR_FOOT", contactNormal, mu, 1, 1000)
+contactFR.setKp(kp_contact * np.ones(6).T)
+contactFR.setKd(2*np.sqrt(kp_contact) * np.ones(6).T)
+fr_ref = solo12_wrapper.position(data, ID_FR)
+contactFR.setReference(fr_ref)
+formulation.addRigidContact(contactFR, 10)
+
+contactHR = tsid.ContactPoint("contactHR", solo12_wrapper, "HR_FOOT", contactNormal, mu, 1, 1000)
+contactHR.setKp(kp_contact * np.ones(6).T)
+contactHR.setKd(2*np.sqrt(kp_contact) * np.ones(6).T)
+hr_ref = solo12_wrapper.position(data, ID_HR)
+contactHR.setReference(hr_ref)
+formulation.addRigidContact(contactHR, 10)
+
+contactHL = tsid.ContactPoint("contactHL", solo12_wrapper, "HL_FOOT", contactNormal, mu, 1, 1000)
+contactHL.setKp(kp_contact * np.ones(6).T)
+contactHL.setKd(2*np.sqrt(kp_contact) * np.ones(6).T)
+hl_ref = solo12_wrapper.position(data, ID_HL)
+contactHL.setReference(hl_ref)
+formulation.addRigidContact(contactHL, 10)
+
+
 
 # launch simu
 
@@ -90,8 +131,7 @@ for i in range(N_simu):
     # numerical integration
     v_mean = v[:,i] + 0.5*dt*dv[:,i]
     v[:,i + 1] = v[:,i] + dt * dv[:,i]
-    q[:,i + 1] = pin.integrate(solo12_wrapper.model(), q[:,i], dt * v_mean)
+    q[:,i + 1] = pin.integrate(solo12_wrapper.model(), q[:,i], dt * v[:,i])
     t += dt
-    print(q[2,i])
     solo12.display(q[:,i])
-    time.sleep(1e-4)
+    time.sleep(1e-1)
