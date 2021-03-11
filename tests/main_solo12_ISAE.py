5b57e3264072090212a55a77a80f03fb55660f43
Damien DESCLAUX <Damien.DESCLAUX@student.isae-supaero.fr>
2021-03-11 10:29:13 +0100

    Main solo for test in real life.

--- tests/main_solo12_ISAE.py
+++ tests/main_solo12_ISAE.py
@@ -5,33 +5,53 @@ import math
 from time import clock, sleep
 from solo12_ISAE import Solo12
 
+data = np.load("traj.npz")
 
-q_ref=np.zeros(12)
-v_ref=np.zeros(12)
+t_traj = data['t']
+q_traj = data['q']
+v_traj = data['q_dot']
+kp_traj = data['gains'][:, 0]
+kd_traj = data['gains'][:, 1]
+
 Kp = 0*0.1
 Kd = 0*0.01
-
 
 def example_script(name_interface):
     device = Solo12(name_interface,dt=0.001)
     nb_motors = device.nb_motors
     
-    device.Init(calibrateEncoders=True, q_ref)
+    device.Init(calibrateEncoders=True, q_init=q_traj[0])
 
 
     #CONTROL LOOP ***************************************************
+    i_traj = 0
+    initial_time = clock()
+
     while ((not device.hardware.IsTimeout()) and (clock() < 200)):
         device.UpdateMeasurment()
 
+        # Get elements from trajectory 
+        if clock()-initial_time > t_traj[i_traj] and i_traj+1<len(t_traj):
+            i_traj += 1
+
+        q_ref = q_traj[i_traj]
+        v_ref = v_traj[i_traj]
+        ff_ref = 0
+        # Kp = kp_traj[i_traj]
+        # Kd = kd_traj[i_traj]
+
+        # Compute torque
         q_err = q_ref - device.q_mes
         v_err = v_ref - device.v_mes
-        tau = Kp*q_err + Kd*v_err
+        tau = Kp*q_err + Kd*v_err + ff_ref
+
         device.SetDesiredJointTorque(tau)
         device.SendCommand(WaitEndOfCycle=True)
+
         if ((device.cpt % 100) == 0):
             device.Print()
             for i in range(12):
-                        print(device.hardware.GetMotor(i).GetPosition())
+                print(device.hardware.GetMotor(i).GetPosition())
     #****************************************************************
     
     # Whatever happened we send 0 torques to the motors.
