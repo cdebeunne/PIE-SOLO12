8257d940ce94f92c2c1c974351fb9c71e8b029af
Damien DESCLAUX <Damien.DESCLAUX@student.isae-supaero.fr>
2021-03-11 14:19:08 +0100

    Put on the ground.

--- tests/main_solo12_ISAE.py
+++ tests/main_solo12_ISAE.py
@@ -2,6 +2,7 @@
 import numpy as np
 import argparse
 import math
+import threading
 from time import clock, sleep
 from solo12_ISAE import Solo12
 
@@ -16,12 +17,51 @@ kd_traj = data['gains'][:, 1]
 Kp = 0*0.1
 Kd = 0*0.01
 
+key_pressed = False
+
+def get_input():
+    global key_pressed
+    keystrk = input('Put the robot on the floor and press Enter \n')
+    # thread doesn't continue until key is pressed
+    key_pressed = True
+
+def put_on_the_floor(device, q_init):
+    """Make the robot go to the default initial position and wait for the user
+    to press the Enter key to start the main control loop
+    Args:
+        device (robot wrapper): a wrapper to communicate with the robot
+        q_init (array): the default position of the robot
+    """
+    global key_pressed
+    key_pressed = False
+    Kp_pos = .1
+    Kd_pos = 0.01
+    imax = 3.0
+    pos = np.zeros(device.nb_motors)
+
+    for motor in range(device.nb_motors):
+        pos[motor] = q_init[device.motorToUrdf[motor]] * device.gearRatioSigned[motor]
+    
+    i = threading.Thread(target=get_input)
+    i.start()
+    while not key_pressed:
+        device.UpdateMeasurment()
+        for motor in range(device.nb_motors):
+            ref = Kp_pos*(pos[motor] - device.hardware.GetMotor(motor).GetPosition() -
+                          Kd_pos*device.hardware.GetMotor(motor).GetVelocity())
+            ref = min(imax, max(-imax, ref))
+            device.hardware.GetMotor(motor).SetCurrentReference(ref)
+        device.SendCommand(WaitEndOfCycle=True)
+
+    print("Start the motion.")
+
+
 def example_script(name_interface):
     device = Solo12(name_interface,dt=0.001)
     nb_motors = device.nb_motors
     
     device.Init(calibrateEncoders=True, q_init=q_traj[0])
-
+    put_on_the_floor(device, q_traj[0])
 
     #CONTROL LOOP ***************************************************
     i_traj = 0
